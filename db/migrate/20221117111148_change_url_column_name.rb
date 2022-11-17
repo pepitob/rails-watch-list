ba196ed14168516a2276da64d05a1783f82a5acd
root <hippolytebourban@gmail.com>
2022-11-17 15:47:49 +0100

    backend done

---
+++ db/migrate/20221117111148_change_url_column_name.rb
@@ -0,0 +1,5 @@
+class ChangeUrlColumnName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :movies, :poster, :poster_url
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "rest-client"
+require "open-uri"
+
+API_KEY = "4ea7489ff6d135a86c581b28eedccc24"
+
+response = RestClient.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{API_KEY}"
+top_articles = JSON.parse(response)["results"]
+
+top_articles.first(19).each do |movie|
+  article_url = "https://api.themoviedb.org/3/movie/#{movie['id']}?api_key=#{API_KEY}"
+  response = RestClient.get article_url
+  a = JSON.parse(response)
+  Movie.create(title: a["title"], overview: a["overview"], poster_url: "https://image.tmdb.org/t/p/original#{a["poster_path"]}", rating: a["vote_average"])
+end
