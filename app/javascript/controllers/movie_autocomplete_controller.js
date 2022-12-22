4e51d0d4eceb70748a6928eeff69c61a6bdb8074
Hippolyte Bourban <hippolytebourban@gmail.com>
2022-12-22 16:03:33 +0100

    connected stimulus controller with simple form

--- app/javascript/controllers/movie_autocomplete_controller.js
+++ app/javascript/controllers/movie_autocomplete_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="movie-autocomplete"
 export default class extends Controller {
+  static targets = ["form"]
+
   connect() {
     console.log("movie autocomplete controller connected")
   }
