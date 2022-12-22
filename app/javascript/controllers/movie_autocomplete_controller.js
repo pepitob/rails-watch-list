b1f81ac604a34b2b4eec55b574152dfa7bade479
Hippolyte Bourban <hippolytebourban@gmail.com>
2022-12-22 16:50:24 +0100

    fixed action and event for stimulus controller

--- app/javascript/controllers/movie_autocomplete_controller.js
+++ app/javascript/controllers/movie_autocomplete_controller.js
@@ -2,9 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="movie-autocomplete"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "input"]
 
   connect() {
     console.log("movie autocomplete controller connected")
+  }
+
+  search(event) {
+    console.log(event);
+    const inputValue = this.inputTarget.value
+    console.log(inputValue)
   }
 }
