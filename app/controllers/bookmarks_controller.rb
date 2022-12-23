acce42b948ce78b37527a008da607f845acc6ad5
Hippolyte Bourban <hippolytebourban@gmail.com>
2022-12-23 08:28:55 +0100

    finished the autocomplete and new movie creation from the moviedb

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "rest-client"
+
 class BookmarksController < ApplicationController
   before_action :set_list, only: %i[new create]
 
@@ -11,6 +14,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
+      flash[:alert] = "This movie already has a bookmark in this list."
       render :new, status: :unprocessable_entity
     end
   end
@@ -28,6 +32,16 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
+    movie_title = params[:bookmark][:movie]
+    if Movie.find_by(title: movie_title)
+      @movie = Movie.find_by(title: movie_title)
+    else
+      movie_url = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV["MOVIEDB_API_KEY"]}&query=#{movie_title}"
+      response = RestClient.get movie_url
+      data = JSON.parse(response)["results"].first()
+      @movie = Movie.create(title: data["title"], overview: data["overview"], poster_url: "https://image.tmdb.org/t/p/original#{data["poster_path"]}", rating: data["vote_average"])
+    end
+    params[:bookmark][:movie_id] = @movie.id
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
